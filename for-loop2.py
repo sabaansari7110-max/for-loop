23aa972e7fd7d8a92eb4e47b6ac6dd1718f736ad
Saba Ansari <sabaansari7110@gmail.com>
2026-02-03 18:34:25 +0530

    Add files via upload

---
+++ for-loop2.py
@@ -0,0 +1,10 @@
+#greets all person names starts with letter(s) in a list 
+
+l= ["harry", "soham", "sachin", "rahul"]
+
+for name in l:
+    if(name.startswith("s")):
+        print(f"hello {name}")
+
+
+
