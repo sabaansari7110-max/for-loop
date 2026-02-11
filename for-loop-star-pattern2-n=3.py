10eb898fb0041d060d05ec1090ad85bd9ac95973
Saba Ansari <sabaansari7110@gmail.com>
2026-02-11 14:51:22 +0530

    Add files via upload

---
+++ for-loop-star-pattern2-n=3.py
@@ -0,0 +1,12 @@
+# n= int(input("enter a number"))
+
+# for i in range(1, n+1):
+#     print(" " * (i- n), end= " ")
+#     print(("*") * (i-1), end= " ")
+#     print("\n")
+
+
+n= int(input("enter a number"))
+for i in range(1, n+1):
+    print("*" * i, end= " ")
+    print("\n")
